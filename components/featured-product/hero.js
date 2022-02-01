1e119cc0044d1ed35b17e0f35ea7da727f23bcbc
pagnito <pagnito@gmail.com>
2022-02-01 00:00:04 -0500

    started on featured-product

--- components/featured-product/hero.js
+++ components/featured-product/hero.js
@@ -1,5 +1,9 @@
 import React from 'react';
+ 
+export default function FeaturedProduct(props){ 
+  return (
+    <div className='z-20 relative w-full h-screen -mt-5 clip-path-one bg-white mb-20'>
 
-export default function HeroFeaturedProduct() {
-  return <div></div>;
-}
+   </div>
+   )
+ }
--- pages/home/home.js
+++ pages/home/home.js
@@ -1,12 +1,14 @@
 import React from "react";
 import EdgyCarousel from "../../components/landing-carousel/edgy";
 import Incentives from "../../components/banner-row/incentives";
+import FeaturedProduct from "../../components/featured-product/hero";
 export default function Home(props) {
   
   return (
     <div className="home ">
       <EdgyCarousel />
       <Incentives />
+      <FeaturedProduct />
     </div>
   );
 }
