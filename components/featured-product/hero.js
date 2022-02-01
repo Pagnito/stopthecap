432e1c0d14cf322478cbd9aed6cdb34a88348661
pagnito <pagnito@gmail.com>
2022-02-01 00:08:04 -0500

    small fixes

--- components/featured-product/hero.js
+++ components/featured-product/hero.js
@@ -2,7 +2,7 @@ import React from 'react';
  
 export default function FeaturedProduct(props){ 
   return (
-    <div className='z-20 relative w-full h-screen -mt-5 clip-path-one bg-white mb-20'>
+    <div className='z-20 relative w-full h-screen -mt-3 clip-path-one bg-white mb-20'>
 
    </div>
    )
