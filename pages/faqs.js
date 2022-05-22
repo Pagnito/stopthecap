ebdfeb92417f7a007854aa1b545f6645c8ea0408
pagnito <pagnito@gmail.com>
2022-05-22 16:32:41 -0400

    fixed faqs bug

---
+++ pages/faqs.js
@@ -0,0 +1,9 @@
+import React from 'react';
+ 
+export default function Faqs(props){ 
+  return (
+    <div className='min-h-screen flex justify-center'>
+      <div className='mt-10'>FAQs</div>
+   </div>
+   )
+ }
